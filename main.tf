eeb90d475a8d6c14998931f668ec4ae8e13dabcd
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-02-23 12:48:39 +0530

    added main.tf, vars.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+
+resource "google_project" "main" {
+  name = "my-proj"
+  project_id = "my-proj-id"
+  org_id = "809866736880"
+  folder_id = ""
+  billing_account = ""
+  labels {}
+}
---
+++ vars.tf
@@ -0,0 +1,32 @@
+# variable "proj-name" {
+# description = "name of the project"
+# type = string
+# }
+
+# variable "project_id" {
+#   description = "Project ID"
+#   type = string
+# }
+
+# variable "org_id" {
+#   description = "org ID"
+#   type = string
+#   default = ""
+# }
+
+# variable "folder_id" {
+#   description = "FolderID"
+#   default = ""
+# }
+
+# variable "billing_account" {
+#   description = "Billing Account"
+#   default = ""
+#   type = string
+# }
+
+# variable "labels" {
+#   type = map(string)
+#   default = {}
+
+# }
